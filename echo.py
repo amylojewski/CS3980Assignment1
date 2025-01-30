ae2ef6ebd80582fcd02861806375931b7954e078
Amy Lojewski <amy-lojewski@uiowa.edu>
2025-01-30 16:47:06 -0600

    begin assignment

---
+++ echo.py
@@ -0,0 +1,9 @@
+# echo.py
+
+def echo(text: str, repetitions: int = 3) -> str:
+    """Imitate a real-world echo"""
+    pass # replace this line with your code lines
+
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
