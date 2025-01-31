0f773560aac4dac2308cb69fd90af5b3214977fe
Amy Lojewski <amy-lojewski@uiowa.edu>
2025-01-30 19:43:16 -0600

    question 1 attempt

--- echo.py
+++ echo.py
@@ -3,13 +3,19 @@
 
 def echo(text: str, repetitions: int = 3) -> str:
     """Imitate a real-world echo"""
-    words = text.split()
-    final_word = words[-1] if words else ""
+    final_word = text.split()[-1] if text.split() else ""
 
-    echo_lines = [final_word[i:] for i in range(min(repetitions, len(final_word)))]
+    echo_lines = [
+        final_word[-i:] for i in range(min(repetitions, len(final_word)), 0, -1)
+    ]
     echo_lines.append(".")
 
     return "\n".join(echo_lines)
+
+
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
 
 
 if __name__ == "__main__":
