5f4d7555f4b372f32ddd83e16cc0fd75af6b5fbd
Amy Lojewski <amy-lojewski@uiowa.edu>
2025-01-30 19:40:10 -0600

    question 1 attempt

--- echo.py
+++ echo.py
@@ -1,8 +1,16 @@
 # echo.py
 
+
 def echo(text: str, repetitions: int = 3) -> str:
     """Imitate a real-world echo"""
-    pass # replace this line with your code lines
+    words = text.split()
+    final_word = words[-1] if words else ""
+
+    echo_lines = [final_word[i:] for i in range(min(repetitions, len(final_word)))]
+    echo_lines.append(".")
+
+    return "\n".join(echo_lines)
+
 
 if __name__ == "__main__":
     text = input("Yell something at a mountain: ")
